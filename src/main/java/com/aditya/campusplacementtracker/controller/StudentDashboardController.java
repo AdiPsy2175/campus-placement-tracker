eb65696cbd139dbad8bd854aa6cf2b9df35de004
AdiPsy2175 <adipsy2175@gmail.com>
2026-07-26 04:06:46 +0530

    Implemented role-based login redirection and student dashboard

--- src/main/java/com/aditya/campusplacementtracker/controller/StudentDashboardController.java
+++ src/main/java/com/aditya/campusplacementtracker/controller/StudentDashboardController.java
@@ -1,16 +1,21 @@
 package com.aditya.campusplacementtracker.controller;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class StudentDashboardController {
 
     @GetMapping("/student/dashboard")
-    public String studentDashboard() {
+    public String studentDashboard(Authentication authentication,
+                                   Model model) {
+
+        model.addAttribute("username",
+                authentication.getName());
 
         return "student-dashboard";
-
     }
 
 }
